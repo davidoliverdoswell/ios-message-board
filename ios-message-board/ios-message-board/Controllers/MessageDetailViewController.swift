25b5052f02696112ddb987f93a996d1f14e8e006
Lambda-School-Loaner-11 <lambda-school-loaner-11@Lambda-School-Loaner-11s-MacBook-Pro.local>
2018-08-08 16:17:50 -0700

    set up MessageDetailViewController storyboard

---
+++ ios-message-board/ios-message-board/Controllers/MessageDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MessageDetailViewController.swift
+//  ios-message-board
+//
+//  Created by Lambda-School-Loaner-11 on 8/8/18.
+//  Copyright © 2018 David Doswell. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBAction func sendButton(_ sender: Any) {
+    }
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
