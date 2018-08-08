bc5c76a50f7ecff0a4e5d177ea733df5d1c0e2a7
Lambda-School-Loaner-11 <lambda-school-loaner-11@Lambda-School-Loaner-11s-MacBook-Pro.local>
2018-08-08 16:08:10 -0700

    set up MessageThreadDetailTableViewController storyboard

---
+++ ios-message-board/ios-message-board/Controllers/MessageThreadDetailTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MessageThreadDetailTableViewController.swift
+//  ios-message-board
+//
+//  Created by Lambda-School-Loaner-11 on 8/8/18.
+//  Copyright © 2018 David Doswell. All rights reserved.
+//
+
+import UIKit
+
+class MessageThreadDetailTableViewController: UITableViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+       
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+}
